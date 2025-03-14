529b1ab44b2bad6853f80612a062224565ff0078
Georgiy Zakirov <granmawinter@icloud.com>
2025-03-14 18:20:39 +0400

    Add services

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -3,22 +3,10 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from database.database import DICT_STORIES_RU
 from keyboards.keyboards import keyboard
-from random import choice
+from services.services import generate_story
 
 
 router = Router()
-
-char_traits = choice(DICT_STORIES_RU['character_traits']).lower()
-char_race = choice(DICT_STORIES_RU['race_list'])
-char_prof = choice(DICT_STORIES_RU['profession_list'])
-char_appearance = choice(DICT_STORIES_RU['appearance_features'])
-char_features = choice(DICT_STORIES_RU['features'])
-char_prehistories = choice(DICT_STORIES_RU['pre_histories'])
-char_motivation = choice(DICT_STORIES_RU['motivation'])
-char_phobias = choice(DICT_STORIES_RU['phobias'])
-
-
-
 
 
 @router.message(CommandStart())
@@ -33,5 +21,5 @@ async def process_help_command(message: Message):
 
 @router.message()
 async def process_generate_backstory(message: Message):
-    await message.answer(text=f"Сегодня ты {char_traits} {char_race} - {char_prof}, {char_appearance}, {char_features}. {char_prehistories}, {char_motivation}. Среди твоих страхов {char_phobias}, но будь отважен и ты сможешь все преодолеть! Вперед! К приключениям!!!")
+    await message.answer(text=generate_story())
 
---
+++ services/services.py
@@ -0,0 +1,17 @@
+from database.database import DICT_STORIES_RU
+from random import choice
+
+
+
+
+def generate_story():
+    char_traits = choice(DICT_STORIES_RU['character_traits']).lower()
+    char_race = choice(DICT_STORIES_RU['race_list'])
+    char_prof = choice(DICT_STORIES_RU['profession_list'])
+    char_appearance = choice(DICT_STORIES_RU['appearance_features'])
+    char_features = choice(DICT_STORIES_RU['features'])
+    char_prehistories = choice(DICT_STORIES_RU['pre_histories'])
+    char_motivation = choice(DICT_STORIES_RU['motivation'])
+    char_phobias = choice(DICT_STORIES_RU['phobias'])
+
+    return f"Сегодня ты {char_traits} {char_race} - {char_prof}, {char_appearance}, {char_features}. {char_prehistories}, {char_motivation}. Среди твоих страхов {char_phobias}, но будь отважен и ты сможешь все преодолеть! Вперед! К приключениям!!!"
